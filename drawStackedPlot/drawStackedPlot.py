93b88434df0dee03b2380ccca4f012544678bed0
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-09-20 10:00:31 +0200

    kosmetyka

--- drawStackedPlot/drawStackedPlot.py
+++ drawStackedPlot/drawStackedPlot.py
@@ -110,6 +110,7 @@ def draw_stackPlot(
 
     axes = plt.gca()
     axes.set_ylim([0, maxVal * 1.2])
+    axes.set_xlim([-1, len(groups_names) + 1])
 
     handles1: [mpatches.Patch] = []
     for i in range(len(order_molecules)):
