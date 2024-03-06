6e958b10118ce65316ce48b5228ff689207de4ac
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2024-03-06 11:31:20 +0100

    small corrections/adjustments/improvements to new MEN list

--- journalQueries/queryForJournals.py
+++ journalQueries/queryForJournals.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 df = pd.read_csv("./listOfJournals.csv")
 
-# columns with discipline names:
-# 302 - nauki farmaceutyczne; 303- nauki medyczne
-disciplinesAnd = ["303"]
+# columns with discipline names (check it with listOfJournals.csv file):
+# 301 - nauki farmaceutyczne; 302- nauki medyczne
+disciplinesAnd = ["302"]
 keywordsIncludeOr = [
     "diabet",
     "obes",
@@ -26,10 +26,10 @@ def isDiscipline(row, disciplinesAnd=disciplinesAnd):
 
 def isOneOfTheKeywords(row, keywordsOr=keywordsIncludeOr):
     for keyword in keywordsOr:
-        if keyword in str(row["tytul1"]).lower():
+        if keyword in str(row["Tytuł 1"]).lower():
             return True
     for keyword in keywordsOr:
-        if keyword in str(row["tytul2"]).lower():
+        if keyword in str(row["Tytuł 2"]).lower():
             return True
     return False
 
@@ -42,17 +42,25 @@ def areJounalTitleCriteriaFullfilled(row):
     return isOneOfTheKeywords(row) and not isOneOfExcludedKeywords(row)
 
 
+def isMenPoints(row, pts):
+    # print("testing ", row["Punkty"], "in", pts, "?", row["Punkty"] in pts)
+    return row["Punktacja"] in pts
+
+
 ###############################################################################
 #                                    query1                                   #
 #           MNISW: 140, discipline: nauki medyczne i farmaceutyczne           #
 ###############################################################################
-df140_mnisw = df[df["Punkty"] == 140]
-rowsDisciplinesTrue = list(df140_mnisw.apply(isDiscipline, axis=1))
-df140_mnisw_disciplines = df140_mnisw[rowsDisciplinesTrue]
+df_w_men_pts = df[list(df.apply(isMenPoints, axis=1, pts=[140, 100]))]
+rowsDisciplinesTrue = list(df_w_men_pts.apply(isDiscipline, axis=1))
+df_w_men_pts_and_disciplines = df_w_men_pts[rowsDisciplinesTrue]
 rowsKeywordsOK = list(
-    df140_mnisw_disciplines.apply(areJounalTitleCriteriaFullfilled, axis=1)
+    df_w_men_pts_and_disciplines.apply(areJounalTitleCriteriaFullfilled,
+                                       axis=1)
 )
-df140_mnisw_disciplines_keywords = df140_mnisw_disciplines[rowsKeywordsOK]
-df140_mnisw_disciplines_keywords.to_csv(
+df_w_men_pts_and_disciplines_and_keywords = df_w_men_pts_and_disciplines[
+    rowsKeywordsOK]
+
+df_w_men_pts_and_disciplines_and_keywords.to_csv(
     "./journals_candidates_query1.csv", index=False, header=True
 )
